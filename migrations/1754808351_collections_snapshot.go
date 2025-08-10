121c90aeb1086d7bee634377ffa88cffebd83c9f
lukivan8 <lukivan888@gmail.com>
2025-08-10 12:19:07 +0500

    change to rewrite

--- migrations/1754808351_collections_snapshot.go
+++ migrations/1754808351_collections_snapshot.go
@@ -1339,7 +1339,7 @@ func init() {
 			}
 		]`
 
-		return app.ImportCollectionsByMarshaledJSON([]byte(jsonData), false)
+		return app.ImportCollectionsByMarshaledJSON([]byte(jsonData), true)
 	}, func(app core.App) error {
 		return nil
 	})
