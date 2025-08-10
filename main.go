e5e88cd3c5a530db9dfecbdb81ec7959cda39ced
lukivan8 <lukivan888@gmail.com>
2025-08-10 12:48:13 +0500

    remove auto migration

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"log"
 	"os"
-	"strings"
 
 	_ "uley_be/migrations"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
@@ -20,14 +18,6 @@ func main() {
 		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
 
 		return se.Next()
-	})
-
-	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
-
-	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
-		// enable auto creation of migration files when making collection changes in the Dashboard
-		// (the isGoRun check is to enable it only during development)
-		Automigrate: isGoRun,
 	})
 
 	if err := app.Start(); err != nil {
